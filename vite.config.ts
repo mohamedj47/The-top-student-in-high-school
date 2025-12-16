2eddeb4231bfe2e8146a6946f970dfd934d47ed4
mohamedj47 <mohammedjana74@gmail.com>
2025-12-16 13:32:42 +0200

    Fix cwd usage in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
   
   // Helper to safely get env var from either process.env (Vercel) or .env file (Local)
   const getEnv = (key: string) => {
